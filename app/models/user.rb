e71002a4106c2555dc4d7ca523366caf965c3b5f
vgartg <gopik539@mail.ru>
2024-07-18 10:05:48 +0300

    Описал некий первичный тест для проверки "дружеских отношений", при этом добавил удаление всех отношений при удалении аккаунта

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
   has_secure_password
   has_one_attached :avatar
   has_many :games, foreign_key: "author_id", dependent: :destroy
-  has_many :friendships
-  has_many :friends, through: :friendships
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships, dependent: :destroy
 
-  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
-  has_many :inverse_friends, through: :inverse_friendships, source: :user
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  has_many :inverse_friends, through: :inverse_friendships, source: :user, dependent: :destroy
 
   def password_length
     if password.nil? || password.length < 5
