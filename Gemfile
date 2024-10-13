590b923735e7b5866de87b243842ffb668fe9642
vgartg <gopik539@mail.ru>
2024-10-13 22:08:57 +0300

    Пофиксил баг с повторением сессий и добавил отображение браузера, с которого они были созданы

--- Gemfile
+++ Gemfile
@@ -55,6 +55,8 @@ gem 'browser'
 
 gem 'flatpickr'
 
+gem 'useragent'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_12_214552) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_13_185805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_12_214552) do
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "friend_id", null: false
-    t.string "status", default: "pending", null: false
+    t.string "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -109,6 +109,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_12_214552) do
     t.string "ip_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "browser"
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
