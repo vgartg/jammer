3047148d2155fc5a5254aaff17991e95facecf94
wolwer1ne <oganesyan@hey.com>
2024-10-14 12:06:14 +0300

    Замигрировал

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_13_094010) do
-
+ActiveRecord::Schema[8.0].define(version: 2024_10_13_185805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
