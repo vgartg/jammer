6532a05e418d86d9bfb4a58c9a3d79664a280aa5
vgartg <gopik539@mail.ru>
2024-10-16 23:30:09 +0300

    Продолжение предыдущего коммита: добавление информирования о том, если какой-то из каталогов пуст / не найден + снова мелкие правки уведомлений и редиректов

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -46,14 +46,14 @@ class GamesController < ApplicationController
     @game = Game.new(game_params.merge(author: current_user))
     @tags = Tag.all
     if @game.save
-      flash[:success] = "Successfully saved!"
+      flash[:success] = "Игра успешно создана!"
       redirect_to dashboard_path
     else
       flash[:failure] = @game.errors.full_messages
       render :new, status: :see_other
     end
   rescue ActiveRecord::RecordNotUnique => e
-    flash[:failure] = "Игра с таким названием уже существует"
+    flash[:failure] = "Игра с таким названием уже существует."
     render :new, status: :see_other
   end
 
@@ -65,7 +65,8 @@ class GamesController < ApplicationController
   def update
     @game = current_user.games.find_by_id(params[:id])
     if @game.update(game_params)
-      redirect_to game_profile_path, notice: 'Игра успешно обновлена'
+      flash[:success] = "Игра успешно обновлена."
+      redirect_to game_profile_path
     else
       flash[:failure] = @game.errors.full_messages
       render :edit, status: :unprocessable_entity
@@ -74,7 +75,11 @@ class GamesController < ApplicationController
 
   def destroy
     @game = current_user.games.find_by_id(params[:id])
-    @game.destroy
+    if @game.destroy
+      flash[:success] = 'Игра успешно удалена.'
+    else
+      flash[:failure] = "Something went wrong!"
+    end
     redirect_to dashboard_path
   end
 
--- app/controllers/jams_controller.rb
+++ app/controllers/jams_controller.rb
@@ -46,13 +46,14 @@ class JamsController < ApplicationController
     @jam = Jam.new(jam_params.merge(author: current_user))
     @tags = Tag.all
     if @jam.save
+      flash[:success] = 'Джем успешно создан!'
       redirect_to dashboard_path
     else
       flash[:failure] = @jam.errors.full_messages
       render :new, status: :see_other
     end
   rescue ActiveRecord::RecordNotUnique => e
-    flash[:failure] = ["Джем с таким названием уже существует"]
+    flash[:failure] = "Джем с таким названием уже существует."
     render :new, status: :see_other
   end
 
@@ -64,7 +65,7 @@ class JamsController < ApplicationController
   def update
     @jam = current_user.jams.find_by_id(params[:id])
     if @jam.update(jam_params)
-      redirect_to jam_profile_path, notice: 'Джем успешно обновлен'
+      redirect_to jam_profile_path, notice: 'Джем успешно обновлен.'
     else
       flash[:failure] = @jam.errors.full_messages
       render :edit, status: :unprocessable_entity
@@ -73,7 +74,11 @@ class JamsController < ApplicationController
 
   def destroy
     @jam = current_user.jams.find_by_id(params[:id])
-    @jam.destroy
+    if @jam.destroy
+      flash[:success] = 'Джем успешно удален.'
+    else
+      flash[:failure] = "Something went wrong!"
+    end
     redirect_to :dashboard
   end
 
