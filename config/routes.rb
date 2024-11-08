9c04ee02b3d1d347ea4841cd8a4aadf2ff1ce5f1
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2024-11-08 20:06:27 +0300

    Фиксы после слияния веток

--- config/routes.rb
+++ config/routes.rb
@@ -79,23 +79,9 @@ Rails.application.routes.draw do
   end
 
   # Games
-  get '/games/new', to: 'games#new'
-  post '/games', to: 'games#create'
-  get '/games/:id/edit', to: 'games#edit', as: 'game_edit'
-  patch '/games/:id', to: 'games#update', as: 'game_update'
-  delete '/games/:id', to: 'games#destroy', as: 'game_destroy'
-  get '/games/:id', to: "games#show", as: 'game_profile'
-  get '/games_showcase', to: 'games#showcase'
   post '/games/:id/submit', to: 'games#submit', as: 'submit'
 
   # Jams
-  get '/jams/new', to: 'jams#new'
-  post '/jams', to: 'jams#create'
-  get '/jams/:id/edit', to: 'jams#edit', as: 'jam_edit'
-  patch '/jams/:id', to: 'jams#update', as: 'jam_update'
-  delete '/jams/:id', to: 'jams#destroy', as: 'jam_destroy'
-  get '/jams/:id', to: "jams#show", as: 'jam_profile'
-  get '/jams_showcase', to: 'jams#showcase'
   get '/jams/:id/submit_game', to: 'jams#submit_game', as: 'game_submission'
   post '/jams/:id/submit_game', to: 'jams#create_submission'
   get '/jams/:id/show_projects', to: 'jams#show_projects', as: 'jam_show_projects'
