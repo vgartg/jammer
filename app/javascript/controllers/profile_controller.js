fa1f94c34dd7f59fe5a4e12977571efc89ac6d65
danilgordienko <danilgordienko212@gmail.com>
2024-11-11 22:10:43 +0300

    добавил подсветку выбранного элемента в профиле

--- app/javascript/controllers/profile_controller.js
+++ app/javascript/controllers/profile_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-    static targets = ["tab-name"];
+    static targets = ["tabName"];
 
 
     openTab(event) {
         const tabName = event.currentTarget.dataset.tabName;
         let i, tab_content, tab_links;
+
         tab_content = document.getElementsByClassName("tab_content");
         for (i = 0; i < tab_content.length; i++) {
             tab_content[i].style.display = "none";
@@ -14,25 +15,11 @@ export default class extends Controller {
 
         tab_links = document.getElementsByClassName("tab_links");
         for (i = 0; i < tab_links.length; i++) {
-            tab_links[i].className = tab_links[i].className.replace(" active", "");
+            tab_links[i].classList.remove("bg-gray-100");
         }
 
         document.getElementById(tabName).style.display = "block";
-        event.currentTarget.className += " active";
-
-        if (tabName === 'user_info') {
-            document.getElementById("btnUserInfo").classList.add("bg-blue-700");
-            document.getElementById("btnUserFriends").classList.remove("bg-blue-700");
-            document.getElementById("btnUserFollowers").classList.remove("bg-blue-700");
-        } else if (tabName === 'user_friends') {
-            document.getElementById("btnUserFriends").classList.add("bg-blue-700");
-            document.getElementById("btnUserInfo").classList.remove("bg-blue-700");
-            document.getElementById("btnUserFollowers").classList.remove("bg-blue-700");
-        }
-        else if (tabName === 'user_followers') {
-            document.getElementById("btnUserFollowers").classList.add("bg-blue-700");
-            document.getElementById("btnUserInfo").classList.remove("bg-blue-700");
-            document.getElementById("btnUserFriends").classList.remove("bg-blue-700");
-        }
+        event.currentTarget.className += " bg-gray-100";
     }
 }
+
