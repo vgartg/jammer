a3483a1b54f36eb056b85a5e68365d5d9cf6f50b
Sergei <aserka59tom@gmail.com>
2024-11-17 15:10:40 +0300

    добавил удаление оценки, добавил отображение оценок в дашборде и в играх, исправил отображение звездочек для дробных, пофиксил баг со средней оценкой

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,17 +1,14 @@
 class Rating < ApplicationRecord
   def self.update_average_rating(game, jam_id)
     if jam_id
-
-      reviews = game.reviews.where(game_id: game.id, jam_id: jam_id)
-      average_rating = reviews.average(:user_mark).to_f.round(1)
+      reviews = game.reviews.where(game_id: game.id, jam_id: jam_id).where.not(user_mark: 0)
+      average_rating = reviews.any? ? reviews.average(:user_mark).to_f.round(1) : 0.0
       game.ratings.find_or_create_by(jam_id: jam_id).update(average_rating: average_rating)
     else
-      reviews = game.reviews.where(game_id: game.id, jam_id: nil)
-      overall_average_rating = reviews.average(:user_mark).to_f.round(1)
+      reviews = game.reviews.where(game_id: game.id, jam_id: nil).where.not(user_mark: 0)
+      overall_average_rating = reviews.any? ? reviews.average(:user_mark).to_f.round(1) : 0.0
       game.ratings.find_or_create_by(jam_id: nil).update(average_rating: overall_average_rating)
     end
   end
 
-
 end
-
