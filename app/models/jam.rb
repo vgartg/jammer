191d4987fba9049e468a751905baf1f5dbe26b6e
Nikita <113203618+Fiitovec99@users.noreply.github.com>
2024-11-18 18:45:17 +0300

    добавлен action text для описания джемы, но нужно поработать над внешним видом

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -1,7 +1,9 @@
 class Jam < ActiveRecord::Base
-  validates :name, :description, :start_date, :deadline, :end_date, presence: true
+  validates :name, :start_date, :deadline, :end_date, presence: true
   validates :cover, presence: { message: "Обложка обязательна для загрузки" }
   validates :logo, presence: { message: "Логотип обязателен для загрузки" }
+
+  has_rich_text :description
 
   has_one_attached :cover
   has_one_attached :logo
@@ -13,4 +15,13 @@ class Jam < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   validates_length_of :tags, maximum: 10, message: "Можно выбрать не более 10 тегов"
+
+  validate :description_presence
+
+  private
+  def description_presence
+    if description.blank? || description.body.blank?
+      errors.add(:description, "Описание обязательно для заполнения")
+    end
+  end
 end
