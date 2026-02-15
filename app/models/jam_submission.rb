0ae22dd164f6dd354bf78d59ef92e46a30717fe2
vgartg <gopik539@mail.ru>
2026-02-15 14:49:34 +0300

    Добавил оценки в рамках джема и удаление проектов/участников от имени создателя джема, исправил пару визуальных моментов

--- app/models/jam_submission.rb
+++ app/models/jam_submission.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 class JamSubmission < ActiveRecord::Base
-  #belongs_to :game
+  belongs_to :game
   belongs_to :jam
   belongs_to :user
-
-  def create
-    @submission = JamSubmission.new(game_id: params[:game_id], jam_id: params[:jam_id])
-    @submission.save
-  end
 end
